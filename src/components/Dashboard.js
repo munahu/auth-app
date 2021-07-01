53f96156542b02e0e107e7430a4a4de3f1146791
Muna Hussen <munahussen@Munas-MBP.lan>
2021-06-30 21:47:09 -0400

    Added styles to Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 import { useEffect, useState } from 'react';
 import useFirebaseAuth from '../hooks/useFirebaseAuth';
+import styles from '../styles/Dashboard.module.css';
 
 function Dashboard() {
     const { signOut } = useFirebaseAuth();
@@ -11,30 +12,38 @@ function Dashboard() {
 
     useEffect(() => {
         if (user) {
-            function getUsername() {
-                const userRef = firebase.database().ref("Users/");
-          
-                userRef.on('value', function(snapshot) {
-                  snapshot.forEach(function(childSnapshot) {
-                    var childData = childSnapshot.val();
-                    const userId = childData.userId;
-                    if (userId === user.uid) {
-                      setName(childData.name)
-                    }
-                  });
-                });
-              }
-
             getUsername();
         }
     }, [user])
 
+    function getUsername() {
+        const userRef = firebase.database().ref("Users/");
+        userRef.on('value', function(snapshot) {
+          snapshot.forEach(function(childSnapshot) {
+            var childData = childSnapshot.val();
+            const userId = childData.userId;
+            if (userId === user.uid) {
+              capitalizeName(childData.name)
+            }
+          });
+        });
+      }
+
+      function capitalizeName(name) {
+        const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+        setName(capitalizedName);
+      }
+
+
     return (
-        <div>   
-           <form onSubmit={signOut}>
-               <button>Sign out</button>
-               {name && <h1>Hello {name}</h1>}
-           </form>
+        <div className={styles.dashboard}>  
+            {name && 
+            <form onSubmit={signOut}>
+                <h1 className={styles.heading}>{name}, you're now logged in!</h1>
+                <h2 className={styles.subheading}>There's nothing to see quite yet. But stay tuned, I'm building an exciting chat app.</h2>
+                <button className={styles.signOut}>Sign out</button>
+            </form>
+            } 
         </div>
     )
 }
